bc0e726da2ce4cd00ebdfb399fca92430511d816
xpesh <xpesh>
2019-02-27 15:04:05 +0400

    one done

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -1,12 +1,26 @@
 package example;
-import mpi.MPI;
+        import mpi.MPI;
 
 public class Main {
     public static void main(String[] args) {
+        int rank, size;
+        double startwtime, endwtime;
+        String name;
+
         MPI.Init(args);
-        int me = MPI.COMM_WORLD.Rank();
-        int size = MPI.COMM_WORLD.Size();
-        System.out.println("Hi from <"+me+">");
+        startwtime = MPI.Wtime();
+        rank = MPI.COMM_WORLD.Rank();
+        size = MPI.COMM_WORLD.Size();
+        name = MPI.Get_processor_name();
+//        load();
+        endwtime = MPI.Wtime();
+        System.out.println(String.format("Hello world from process %d of %d at %s as %f second",rank, size, name, endwtime - startwtime));
         MPI.Finalize();
+    }
+
+    private static void load(){
+        for(long i=0;i<20000000L;i++){
+            Math.sqrt(System.currentTimeMillis());
+        }
     }
 }
